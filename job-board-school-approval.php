e55241026e923c71971e49ee69e4a389776398ad
jaikun1 <jaicelim@gmail.com>
2016-08-28 01:07:09 +0800

    107am

--- job-board-school-approval.php
+++ job-board-school-approval.php
@@ -35,11 +35,9 @@
 				<center>
 					<div id="job-board" class="container">
 					<h3 class="pull-left">Job Board</h3>
-<<<<<<< HEAD
 					<div class="pull-left sub-nav">
-=======
 					<div class="pull-left">
->>>>>>> 4a9a2c0df1145ce52a7395a1b821616af4ffccf8
+
 					<a href="job-board-school.php"><h4 class="pull-left">Approved &nbsp;</h4></a>
 					<h4 class="pull-left">|</h4>
 					<a href="#"><h4 class="pull-left active-page">&nbsp;For Approval</h4>
