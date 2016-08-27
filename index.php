0c18a240db2988ee6745d594d33e0c6002749af3
jaikun1 <jaicelim@gmail.com>
2016-08-27 13:54:12 +0800

    firscommit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans|Quicksand|Roboto" rel="stylesheet"> 
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link type="text/css" rel="stylesheet" href="css/index.css">
+	</head>
+	<body>
+		<video playsinline autoplay muted loop poster="images/indexbg.jpg" id="bgvid">
+    		<source src="videos/index-mov.mp4" type="video/mp4">
+		</video>
+
+		<nav>
+			<h2 class="pull-left">EduBoost</h2>
+			<button class="hollow-btn">LOGIN</button>
+		</nav>
+
+
+
+
+		<script src="bootstrap.min.js"></script>
+		<script src="js/jquery-2.2.0.min.js"></script>
+	</body>
+</html>
---
+++ initview-student.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans|Quicksand|Roboto" rel="stylesheet"> 
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link type="text/css" rel="stylesheet" href="css/nav-student.css">
+		<link type="text/css" rel="stylesheet" href="css/initview-student.css">
+	</head>
+	<body>
+		<nav>
+			
+			
+		</nav>
+			<div class="wrapper">
+				<center>
+					<div id="job-board" class="container">
+					<h3 class="pull-left">Job Board</h3>
+					<form>
+						<input type="text" class="underline-input pull-right" style="margin-top:20px;" placeholder="Search...">
+
+					</form>
+						<ul id="job-list" class="list-unstyled">
+							<li>
+								<div class="container-fluid job-item">
+									<h4 class="pull-left">Sample Job Name</h4>
+									<h4>Category:</h4>
+								</div>
+							</li>
+
+						</ul>
+					</div>
+				</center>
+			</div>
+
+		<script src="bootstrap.min.js"></script>
+		<script src="js/jquery-2.2.0.min.js"></script>
+	</body>
+</html>
