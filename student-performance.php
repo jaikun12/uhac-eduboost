0e0235fbc8e4f16f81873069ea0a801f2cf86395
jaikun1 <jaicelim@gmail.com>
2016-08-28 13:13:32 +0800

    last na po talaga

--- student-performance.php
+++ student-performance.php
@@ -64,9 +64,9 @@
 		<nav>
 			<img class="pull-left logo" style="margin-top:-20px;" src="images/eduboostlogo.png">
 			<ul id="nav-menu" class="list-inline pull-left">
-                        <li><a href="school-dash.php"><div>MY STUDENTS</div></a></li>
+                        <li><a href="school-dash.php"><div class="active">MY STUDENTS</div></a></li>
                         <li><a href="job-board-school.php"><div>JOB BOARD</div></a></li>
-                        <li><a href=""><div class="active">MY PERFORMANCE</div></a></li>
+                        <li><a href=""><div>MY PERFORMANCE</div></a></li>
                         <li><a href=""><div>LINKAGES</div></a></li>
                   </ul>
 			
